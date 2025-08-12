b5671fef9a7ce7fd084e923a8833e98aff4512a4
mohiuddin-san <mohi@san-developer.com>
2025-08-12 14:04:45 +0900

    Add entry.server.tsx for Netlify

---
+++ app/entry.server.tsx
@@ -0,0 +1,7 @@
+import { createRequestHandler } from "@netlify/remix-edge-adapter";
+import * as build from "@remix-run/dev/server-build";
+
+export default createRequestHandler({
+  build,
+  mode: process.env.NODE_ENV,
+});
