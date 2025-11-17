c8bb4e600533140be7c78739d4eefa21f5137ab9
mohiuddin-san <mohi@san-developer.com>
2025-11-17 12:31:42 +0900

    mobile

--- app/root.tsx
+++ app/root.tsx
@@ -40,6 +40,7 @@ export default function App() {
     <html lang="jp">
       <head>
         <meta name="google-site-verification" content="9CLFMsHmfETMAJ4uw926BzK1qBqLEapvtLvsmvVwSgA" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
         <Meta />
         <Links />
